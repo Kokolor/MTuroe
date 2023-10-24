38d0658a244c030873d993c77562fc0b5388c46b
Kokolor <kokolor.mail@gmail.com>
2023-10-24 18:48:41 +0200

    WC

--- app/src/main/java/com/turoepkg/mturoe/MainActivity.java
+++ app/src/main/java/com/turoepkg/mturoe/MainActivity.java
@@ -1,6 +1,7 @@
 package com.turoepkg.mturoe;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,9 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("http://turoe.pythonanywhere.com/");
     }
 }
